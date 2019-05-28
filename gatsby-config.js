9287d757b1068fd67039964368ce38ea0bac5b34
nazakbar <nazrinakbar15@gmail.com>
2019-05-28 17:55:34 +0800

    Added service worker and modified manifest.

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,11 +20,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Concrezan`,
+        short_name: `Concrezan`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#121212`,
+        theme_color: `#121212`,
         display: `minimal-ui`,
         icon: `src/images/concrezan-icon.png`, // This path is relative to the root of the site.
       },
@@ -39,6 +39,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
