cd807fe645bcae94c874c010aedca5c913c15310
nazakbar <nazrinakbar15@gmail.com>
2019-05-30 21:08:04 +0800

    Added canonical urls to project.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-react-helmet-canonical-urls`,
+      options: {
+        siteUrl: `https://concrezan.com`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
